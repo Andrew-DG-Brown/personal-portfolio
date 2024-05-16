12344c1cc39f593ded311be1e23d3b9a7c3786da
andrew-brown-hubio <andrew.brown@hubio.com>
2024-05-16 20:30:03 +0200

    Smooth scroll to top

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -6,14 +6,13 @@ function ScrollToTop() {
     if (!scrolled) return null;
     
     return (
-        <a
-        href="/"
+        <button onClick={() => scrollTo({ top: 0, behavior: 'smooth'})}
         className="flex items-center justify-center bg-primary text-white w-10 h-10 rounded-md fixed bottom-8 right-8 left-auto z-[999] hover:shadow-signUp transition duration-300"
             >
             <span
                 className="w-3 h-3 border-t border-l border-white rotate-45 mt-[6px]"
             ></span>
-        </a>
+        </button>
     );
 }
 
