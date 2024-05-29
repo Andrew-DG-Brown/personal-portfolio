759fb6e44afafbde9cd27beaf61fa58773591372
andrew-brown-hubio <andrew.brown@hubio.com>
2024-05-29 11:34:04 +0200

    Quick fix: page transition playing twice, video not covering card, projects not centered

--- src/utils/Transition.tsx
+++ src/utils/Transition.tsx
@@ -1,4 +1,4 @@
-import { motion, useAnimate } from 'framer-motion';
+import { inView, motion, useAnimate } from 'framer-motion';
 import LargeLogo from '../components/LargeLogo';
 
 const bgTransitions = { ease: [0.22, 1, 0.36, 1], duration: 1 }
@@ -7,6 +7,7 @@ const coverScreen = 'fixed top-0 left-0 w-screen h-screen'
 
 export const TransitionAnimation = () => {
     const [scope, animate] = useAnimate()
+    inView(scope.current, () => { logoAnimation() })
 
     const logoAnimation = async () => {
         await animate('svg', { opacity: 1, scale: 1, rotate: 20 }, { duration: 0.4, ease: 'easeOut'})
@@ -19,9 +20,7 @@ export const TransitionAnimation = () => {
             className={`${coverScreen} flex justify-center items-center z-20 pointer-events-none`}>
                 <LargeLogo motionProps={{ style: { opacity: 0, scale: 0 }}} fill='#fff' />
             </motion.div>
-            
             <motion.div key={'fade-in'} className={`${coverScreen} bg-black origin-bottom z-10`} 
-            onAnimationStart={logoAnimation}
             initial={{ scaleY: 0}} animate={{ scaleY: 0}} exit={{ scaleY: 1}} transition={bgTransitions}/>
             <motion.div key={'fade-out'} className={`${coverScreen} bg-black origin-top z-10`} 
             initial={{ scaleY: 1}} animate={{ scaleY: 0}} exit={{ scaleY: 0}} transition={bgTransitions} />
